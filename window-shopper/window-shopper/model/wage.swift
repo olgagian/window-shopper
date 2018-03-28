f88e4267b08ae8c1914bdea1e3c0a43b1219c277
olgagian <ioannis@poleiseis.giannakidis.local>
2018-03-28 23:49:42 +0300

    last commit for now

---
+++ window-shopper/window-shopper/model/wage.swift
@@ -0,0 +1,9 @@
+//
+//  wage.swift
+//  window-shopper
+//
+//  Created by ioannis giannakidis on 28/03/2018.
+//  Copyright © 2018 ioannis giannakidis. All rights reserved.
+//
+
+import Foundation
